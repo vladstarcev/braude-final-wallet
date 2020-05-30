504a5e914ffad72f10faa21029347c39d73cfb97
unknown <vladstarcev@yandex.ru>
2020-05-30 12:18:39 +0300

    added main page

---
+++ test.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const ejs = require('ejs');
+const QRCode = require('qrcode');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.get('/', function(req,res) {
+  QRCode.toDataURL(JSON.stringify('3MaC5uA6ru4XA3S1stficMK5zbVTSgyH5o'), {
+    errorCorrectionLevel: 'H'
+  }, function(err, url) {
+    //console.log(url);
+
+    res.render('main', {
+      qrcode: url
+    });
+  });
+});
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000");
+});
