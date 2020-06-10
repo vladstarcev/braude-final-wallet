272db68cff3d2ae9a17f84c90cd92ea285807adb
unknown <vladstarcev@yandex.ru>
2020-06-10 19:19:17 +0300

    added add wallet

---
+++ public/scripts/main.js
@@ -0,0 +1,15 @@
+$(".change").on("click", function() {
+  $(".change-window").slideToggle();
+  $(".change").toggleClass("btn-primary").toggleClass("btn-outline-primary");
+});
+
+$(".add-select").change(function() {
+  var current = $(".current-wallet").text();
+  console.log(current);
+  var selected = $(".add-select option:selected").text();
+  console.log(selected);
+  if (current == selected) {
+    $(".add-wallet-alert").show();
+    $(".add-wallet-btn").addClass("disabled");
+  }
+});
