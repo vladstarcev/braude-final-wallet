13ba28399fdea07867eb95b9823b2128b4166c70
unknown <vladstarcev@yandex.ru>
2020-06-15 14:17:06 +0300

    login/create validations

--- public/scripts/welcome.js
+++ public/scripts/welcome.js
@@ -1,3 +1,5 @@
+var pass, confirm;
+
 $(".login-btn").on("click", function() {
   $(".login-form").slideToggle();
   $(".create-form").slideUp();
@@ -6,4 +8,17 @@ $(".login-btn").on("click", function() {
 $(".create-btn").on("click", function() {
   $(".create-form").slideToggle();
   $(".login-form").slideUp();
+});
+
+$(".pass").on('input', function(){
+  pass = $('.pass').val();
+});
+
+$(".confirm-pass").on('input', function(){
+  confirm = $(".confirm-pass").val();
+  if(pass && pass == confirm && $('#newUsernameError').css('display') == 'none') {
+    $('.create-submit-btn').prop('disabled', false);
+  } else {
+    $('.create-submit-btn').prop('disabled', true);
+  }
 });
